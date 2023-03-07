a74a4472ce7aa933bfb9506a1674924fe06db832
Nico-Serge <70102638+Nico-Serge@users.noreply.github.com>
2023-03-07 09:45:13 +0100

    Update Vigenere.java

--- src/main/java/Vigenere.java
+++ src/main/java/Vigenere.java
@@ -1,6 +1,6 @@
 public class Vigenere {
 
-    private static char[] alphabet = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+    private static char[] alphabet = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
 
     public static String encrypt(String key, String message) {
         char[] keyChars = key.toCharArray();
